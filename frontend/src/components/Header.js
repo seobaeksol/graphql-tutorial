5a4261b4a016d735486f76b2a2094b3d583ac983
Suyoung Kim <suyoung154@gmail.com>
2020-02-09 13:39:51 +0900

    create search form, no functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,6 +13,10 @@ const Header = props => {
         <Link to="/" className="ml1 no-underline black">
           new
         </Link>
+        <div className="ml1">|</div>
+        <Link to="/search" className="ml1 no-underline black">
+          search
+        </Link>
         {isLoggedIn && (
           <div className="flex">
             <div className="ml1">|</div>
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useQuery } from "urql";
+import gql from "graphql-tag";
+import Link from "./Link";
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Search = () => {
+  const [filter, setFilter] = React.useState("");
+  const [result, executeQuery] = useQuery({
+    query: FEED_SEARCH_QUERY,
+    variables: { filter },
+    pause: true
+  });
+
+  const search = React.useCallback(() => {
+    executeQuery();
+  }, [executeQuery]);
+  const links = result.data ? result.data.feed.links : [];
+
+  return (
+    <div>
+      <div>
+        Search
+        <input type="text" onChange={e => setFilter(e.target.value)} />
+        <button onClick={search}>search</button>
+      </div>
+      {links.map((link, index) => (
+        <Link key={link.id} link={link} index={index} />
+      ))}
+    </div>
+  );
+};
+
+export default Search;
